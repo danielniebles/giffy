d68b4af6b18937fcf8fb3294167f0a0183a8c377
danielniebles <danielf.niebles@gmail.com>
2021-11-28 16:17:49 -0500

    Create searchbar component and add memoization

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'wouter'
 import './Gif.css'
 
-export default function Gif({ title, url, id }) {
+function Gif({ title, url, id }) {
   return (
     <div className="Gif">
       <Link to={`/gif/${id}`} className='Gif-link'>
@@ -13,3 +13,5 @@ export default function Gif({ title, url, id }) {
     </div>
   )
 }
+
+export default React.memo(Gif)
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,21 @@
-import React, { useState } from "react"
+import SearchForm from "components/SearchForm"
+import React, { useCallback } from "react"
 import { useLocation } from "wouter"
 import GifsList from "../../components/GifsList"
 import TrendingSearches from "../../components/TrendingSearches"
 import { useGifs } from "../../hooks/useGifs"
 
 export default function Home(){
-  const [ keyword, setKeyword ] = useState('')
   const [ ,pushLocation ] = useLocation()
   const { gifs } = useGifs()
 
-  const handleSubmit = evt => {
-    evt.preventDefault()
+  const handleSubmit = useCallback(({keyword}) => {
     pushLocation(`search/${keyword}`)
-  }
-
-  const handleInputChange = evt => {
-    setKeyword(evt.target.value)
-  }
+  }, [pushLocation])
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input placeholder="Search a gif..." onChange={handleInputChange} type="text" value={keyword}/>
-      </form>
+      <SearchForm onSubmit={handleSubmit}/>
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última búsqueda</h3>
