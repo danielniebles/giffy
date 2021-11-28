24a4865a26869a0a984d0d70e1d193128d89a8b7
danielniebles <danielf.niebles@gmail.com>
2021-11-28 16:17:06 -0500

    Add lazy load to home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Suspense } from "react"
 import { Link, Route } from "wouter";
 import Home from './pages/Home';
 import SearchResults from './pages/SearchResults';
@@ -6,24 +7,28 @@ import Detail from './pages/Detail';
 import StaticContext from './context/StaticContext';
 import { GifsContextProvider } from './context/GifsContext';
 
+const HomePage = React.lazy(() => import('./pages/Home'))
+
 function App() {
   return (
-    <StaticContext.Provider value={{ name: 'Simona'}}>
+    <StaticContext.Provider value={{ name: 'Simona' }}>
 
-    <div className="App">
-      <section className="App-content">
-      <Link to="/">
-      <figure className="App-logo">
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <figure className="App-logo">
               <img alt='Giffy logo' src='/logo.png' />
-      </figure>
-      </Link>
-      <GifsContextProvider>
-        <Route component={SearchResults} path="/search/:keyword"/>
-        <Route component={Home} path="/"></Route>
-        <Route component={Detail} path="/gif/:id"></Route>
-      </GifsContextProvider>
-      </section>
-    </div>
+            </figure>
+          </Link>
+          <GifsContextProvider>
+            <Route component={SearchResults} path="/search/:keyword" />
+            <Suspense fallback={null}>
+              <Route component={HomePage} path="/"></Route>
+            </Suspense>
+            <Route component={Detail} path="/gif/:id"></Route>
+          </GifsContextProvider>
+        </section>
+      </div>
     </StaticContext.Provider>
   );
 }
